d95fb077f4fa3d462e1a130c3e8a685be4fb5f08
emreatasoy <yunusatasoyy16@gmail.com>
2025-11-30 23:28:35 +0300

    Ready for Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import { useDispatch, useSelector } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { toast } from "react-toastify";
+import { HomeIcon, Info } from "lucide-react";
+
 
 function App() {
 
@@ -13,8 +18,8 @@ function App() {
   return (
     <Router>
       <nav className="p-4 space-x-4 bg-gray-100">
-        <Link to="/" className="text-blue-500">Home</Link>
-        <Link to="/about" className="text-blue-500">About</Link>
+        <Link to="/" className="text-blue-500"><HomeIcon size={20}/>Home</Link>
+        <Link to="/about" className="text-blue-500"><Info size={20}/>About</Link>
       </nav>
 
       <div className="p-4">
@@ -25,11 +30,15 @@ function App() {
          {/* Redux Test */}
         <div className="mt-8 p-4 border rounded">
         <h2 className="text-xl font-bold mb-2">Counter: {count}</h2>
-        <button className="px-4 py-2 bg-blue-500 text-white mr-2" onClick={() => dispatch({type:"INCREMENT"})}>+</button>
+        <button className="px-4 py-2 bg-blue-500 text-white mr-2" onClick={() => {
+  dispatch({ type: "INCREMENT" });
+  toast.success("Sayı artırıldı!");
+}}>+</button>
 
-        <button className="px-4 py-2 bg-red-500 text-white mr-2" onClick={() => dispatch({type:"DECREMENT"} )}>-</button>
-
+        <button className="px-4 py-2 bg-red-500 text-white mr-2" onClick={() => {dispatch({type:"DECREMENT"}); toast.success("Sayı azaltıldı!")}}>-</button>
+<ToastContainer />
         </div>
+        
       </div>
     </Router>
   );
